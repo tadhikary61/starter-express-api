cf92b7e68cf06359b013257332a68e0930116527
Tanmoy Adhikary <98036311+tadhikary61@users.noreply.github.com>
2022-12-06 19:02:56 +0530

    new

--- app.js
+++ app.js
@@ -1,33 +1,8 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const Express = require('express');
 
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-const authRoutes = require('./routes/auth');
-const sequelize = require('./util/database');
-const Product = require('./model/product');
-const User = require('./model/user');
-const Order = require('./model/order');
+const app = Express();
+app.get('/',(req,res,next)=>{
+    res.end("Home Paage")
+})
 
-const app = express();
-
-app.use(bodyParser.json());
-
-app.use(categoryRoutes);
-app.use(productRoutes);
-app.use(authRoutes);
-
-app.use((error, req, res, next) => {
-  res.status(error.statusCode || 500).json({
-    message: error.message,
-  });
-});
-
-Product.belongsToMany(User, { through: Order });
-
-sequelize
-  .sync({ force: true })
-  .then((result) => {
-    app.listen(8080);
-  })
-  .catch((err) => console.log(err));
+app.listen(3000);
--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const authController = require('../controller/auth');
-
-router.post('/signup', authController.signup);
-
-router.post('/login', authController.login);
-
-module.exports = router;
